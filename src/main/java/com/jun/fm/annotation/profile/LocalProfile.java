654894d543771e916fbb03647b17dcc7effb6690
Young Jun Park <june0313@gmail.com>
2017-05-26 18:15:49 +0900

    profile 어노테이션 정의

---
+++ src/main/java/com/jun/fm/annotation/profile/LocalProfile.java
@@ -0,0 +1,11 @@
+package com.jun.fm.annotation.profile;
+
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by wayne on 2017. 5. 26..
+ *
+ */
+@Profile("local")
+public @interface LocalProfile {
+}
---
+++ src/test/java/com/jun/fm/annotation/profile/ActiveTestProfile.java
@@ -0,0 +1,11 @@
+package com.jun.fm.annotation.profile;
+
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * Created by wayne on 2017. 5. 26..
+ *
+ */
+@ActiveProfiles("test")
+public @interface ActiveTestProfile {
+}
