e70b7193af1c5b2629c7bb692fc5615c9c9c807e
Young Jun Park <june0313@gmail.com>
2017-05-26 18:17:26 +0900

    ClubRepository 작성

---
+++ src/main/java/com/jun/fm/repository/ClubRepository.java
@@ -0,0 +1,11 @@
+package com.jun.fm.repository;
+
+import com.jun.fm.domain.club.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by wayne on 2017. 5. 26..
+ *
+ */
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
