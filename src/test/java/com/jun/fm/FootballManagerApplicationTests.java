ee1c816dd4f040fe71cd22f1dd445fc901832dc4
Young Jun Park <june0313@gmail.com>
2017-05-26 13:44:27 +0900

    initial commit

---
+++ src/test/java/com/jun/fm/FootballManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jun.fm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FootballManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
